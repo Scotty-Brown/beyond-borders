28b3f6f48f0afe790bc83e89ea682e8636db6f8b
Scotty Brown <scottybrown98@gmail.com>
2023-08-05 00:49:27 -0500

    Add sample data

---
+++ test/sample-data.js
@@ -0,0 +1,31 @@
+// "travelers": [
+//     {
+//     "id": 1,
+//     "name": "Ham Leadbeater",
+//     "travelerType": "relaxer"
+//     }
+// ]
+
+// "trips": [
+//     {
+//     "id": 1,
+//     "userID": 44,
+//     "destinationID": 49,
+//     "travelers": 1,
+//     "date": "2022/09/16",
+//     "duration": 8,
+//     "status": "approved",
+//     "suggestedActivities": []
+//     }
+// ]
+
+// "destinations": [
+//     {
+//     "id": 1,
+//     "destination": "Lima, Peru",
+//     "estimatedLodgingCostPerDay": 70,
+//     "estimatedFlightCostPerPerson": 400,
+//     "image": "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
+//     "alt": "overview of city buildings with a clear sky"
+//     }
+// ]
