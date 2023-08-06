691f7299a168b910bd7d9676b44171e385ea15c1
Scotty Brown <scottybrown98@gmail.com>
2023-08-06 00:06:44 -0500

    Organize functionality into seperate files

--- src/scripts.js
+++ src/scripts.js
@@ -9,7 +9,8 @@ import './images/bb-NB.png'
 import './images/beyond-borders-logo-NB.png'
 
 import { promises } from './api';
-
+import { getTraveler, getTravelerTrips, getTotalSpentOnTrips } from './data-model';
+import { displayTrips, displayYTDSpend } from './dom-updates';
 const pastTripsContainer = document.querySelector('.past-trips-container')
 
 
@@ -34,75 +35,13 @@ window.addEventListener('load', () => {
         fetchData.destinations = results[2].destinations
         // console.log(fetchData)
     }).then(data => {
-        getTraveler(1)
+        getTraveler(2)
         getTravelerTrips()
     }).then(test => {
         displayTrips()
-        getTripTotal(traveler.trips[0])
+        displayYTDSpend()
     })
 })
 
-////////dataModel//////////
-const getTraveler = (id) => {
-    traveler.info = fetchData.travelers.find((user) => user.id === id)
-    // return traveler
-}
 
-const getTravelerTrips = () => {
-    traveler.trips = fetchData.trips.filter((trip) => trip.userID === traveler.info.id)
-    // return traveler.trips
-}
-
-const getTripTotal = (trip) => {
-    let total = 0
-    const destinationDetails = fetchData.destinations.find((entry) => entry.id === trip.destinationID)
-    let totalCostPerPerson = (destinationDetails.estimatedLodgingCostPerDay * trip.duration) + destinationDetails.estimatedFlightCostPerPerson
-    let tripTotal = trip.travelers * totalCostPerPerson
-    total += tripTotal
-
-    return total
-}
-
-const getTotalSpentOnTrips = (trip) => {
-    const tripCost = traveler.trips.reduce((total, trip) => {
-        // let peopleDay = trip.travelers * trip.duration
-        }, 0)
-        
-}
-
-const createPastTripCardElement = (trip) => {
-    const cardElement = document.createElement('article')
-    cardElement.innerHTML = `
-        <div class="trip-card-header">
-            <h4 class="trip-card-location">Trip Name: ${trip.destinationID}</h4>
-            <img src="https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80" alt="Vacation Image">
-        </div>
-        <div class="trip-card-body">
-        <p class="trip-date">${trip.date}</p>
-        <p class="trip-duration">${trip.duration}</p>
-        <p class="trip-cost">COST</p>
-        </div>
-    `
-
-    // let imgURL = "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80"
-
-    // cardElement.style.backgroundImage = `url(${imgURL})`
-
-    return cardElement
-
-}
-
-
-//////domUpdates/////////
-const displayTrips = () => {
-    const cardContainer = document.getElementById('past-trips-container')
-
-    // console.log(traveler.trips)
-
-    traveler.trips.forEach(element => {
-        const cardElement = createPastTripCardElement(element)
-        cardContainer.appendChild(cardElement)
-    });
-
-}
 
