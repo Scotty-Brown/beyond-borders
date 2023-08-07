0e2ea123509ead93310215e7436be5d7b89b88d1
Scotty Brown <scottybrown98@gmail.com>
2023-08-07 16:52:33 -0500

    Update styling for past trips cards

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,8 +1,17 @@
 import chai from 'chai';
 const expect = chai.expect;
 
-describe('See if the tests are running', function() {
+// import { expect } from 'chai';
+
+import { fetchData, traveler } from '../src/data-model';
+import { getTraveler } from '../src/data-model';
+// import { sampleTravelerData } from './sample-traveler-data';
+
+describe('work lol', function() {
+
+
+
   it('should return true', function() {
-    expect(true).to.equal(true);
+  expect(true).to.equal(true);
   });
 });
