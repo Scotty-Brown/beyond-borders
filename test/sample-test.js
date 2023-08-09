5b5d1fc40d558500c23544cec1650f0d8f344a91
Scotty Brown <scottybrown98@gmail.com>
2023-08-08 21:20:27 -0500

    Add sample data and test dataModel functions

--- test/sample-test.js
+++ test/sample-test.js
@@ -3,15 +3,151 @@ const expect = chai.expect;
 
 // import { expect } from 'chai';
 
-import { fetchData, traveler } from '../src/data-model';
-import { getTraveler } from '../src/data-model';
-// import { sampleTravelerData } from './sample-traveler-data';
+// import { captureFormInput, fetchData, traveler } from '../src/data-model';
+import { getTraveler, getTravelerTrips, getDestinationDetails, getTripTotal, getTotalSpentOnTrips, captureFormInput, fetchData, traveler } from '../src/data-model';
+import { sampleTravelerData } from './sample-traveler-data';
 
-describe('work lol', function() {
+describe('Traveler/Trips Repository', () => {
 
+  beforeEach(() => {
+    fetchData.travelers = sampleTravelerData.travelers
+    fetchData.trips = sampleTravelerData.trips
+  })
 
+  it('Should return user info if user exists', () => {
+    const traveler = getTraveler(1)
+    const traveler2 = getTraveler(2)
 
-  it('should return true', function() {
-  expect(true).to.equal(true);
+    expect(traveler).to.be.an('object');
+    expect(traveler2).to.be.an('object')
+    expect(traveler.name).to.equal('Scotty Brown')
+    expect(traveler2.name).to.equal('Leo Decaprio')
+    expect(traveler.travelerType).to.equal('relaxer')
+    expect(traveler2.travelerType).to.equal('thrill-seeker')
   });
+
+  it('Should return error message if user does not exist', () => {
+
+    const traveler = getTraveler(8, sampleTravelerData)
+    const traveler2 = getTraveler(-1, sampleTravelerData)
+
+    expect(traveler).to.equal('No User Found')
+    expect(traveler2).to.equal('No User Found')
+  })
+
+  it('Should return all user trips', () => {
+    getTraveler(1)
+    const userTrip = getTravelerTrips()
+
+    expect(userTrip).to.be.an('array')
+    expect(userTrip.length).to.equal(2)
+    expect(userTrip[0].duration).to.equal(8)
+  })
+
+  it('Should return error message is user has no trips', () => {
+    getTraveler(2)
+    const userTrip = getTravelerTrips()
+
+    expect(userTrip).to.equal('User has no trips')
+  }) 
 });
+
+describe('Destination Repository', () => {
+
+  let sampleTripOne, sampleTripTwo, invalidTrip
+
+  beforeEach(() => {
+    fetchData.travelers = sampleTravelerData.travelers
+    fetchData.trips = sampleTravelerData.trips
+    fetchData.destinations = sampleTravelerData.destinations
+
+    sampleTripOne = sampleTravelerData.trips[0]
+    sampleTripTwo = sampleTravelerData.trips[1]
+
+    invalidTrip = {
+      "id": 1,
+      "userID": 1,
+      "destinationID": 7,
+      "travelers": 1,
+      "date": "2022/09/16",
+      "duration": 8,
+      "status": "approved",
+      "suggestedActivities": []
+      }
+  })
+
+  it('Should return destination details', () => {
+    let destinationOne = getDestinationDetails(sampleTripOne)
+    let destinationTwo = getDestinationDetails(sampleTripTwo)
+
+    expect(destinationOne).to.be.an('object')
+    expect(destinationTwo).to.be.an('object')
+    expect(destinationOne.destination).to.equal('Gainesville, FL')
+    expect(destinationTwo.estimatedFlightCostPerPerson).to.equal(400)
+  })
+
+  it('Should return error message if destination does not exist', () => {
+    let invalidDestination = getDestinationDetails(invalidTrip)
+
+    expect(invalidDestination).to.equal('Destination Invalid')
+  })
+
+  it('Should return total amount including agent fee for a trip', () => {
+    let tripTotal = getTripTotal(sampleTripOne)
+
+    expect(tripTotal).to.be.a('number')
+    expect(tripTotal).to.equal(2189)
+  })
+
+  it('Should return error message if destination does not exist', () => {
+    let invalidTripRequest = getTripTotal(invalidTrip)
+
+    expect(invalidTripRequest).to.equal('Destination Invalid')
+  })
+
+  it('Should return amount user has spent on all trips', () => {
+
+    getTraveler(1)
+    getTravelerTrips()
+
+    let totalSpent = getTotalSpentOnTrips()
+
+    expect(totalSpent).to.be.a('number')
+    expect(totalSpent).to.equal(11319)
+  })
+
+  it('Should return 0 if user has no prior trips', () => {
+    getTraveler(2)
+    getTravelerTrips()
+    let totalSpent = getTotalSpentOnTrips()
+
+    expect(totalSpent).to.equal(0)
+  })
+})
+
+describe('Create Element', () => {
+
+  beforeEach(() => {
+    getTraveler(2)
+    getTravelerTrips()
+
+    
+  })
+  
+  it('Should return an object for fetch Post', () => {
+    
+    let inputs = {
+      date: "2023/09/16",
+      numNights: 10,
+      numGuests: 2,
+      destination: 2
+    }
+    
+    let formObject = captureFormInput(inputs.date, inputs.numNights, inputs.numGuests, inputs.destination)
+    
+    expect(formObject).to.be.an('object')
+    expect(formObject.duration).to.equal(10)
+    expect(formObject.date).to.equal('2023/09/16')
+  })
+
+})
--- test/sample-traveler-data.js
+++ test/sample-traveler-data.js
@@ -2,33 +2,86 @@ export const sampleTravelerData = {
     travelers: [
         {
         "id": 1,
-        "name": "Ham Leadbeater",
+        "name": "Scotty Brown",
         "travelerType": "relaxer"
         },
         {
         "id": 2,
-        "name": "Rachael Vaughten",
+        "name": "Leo Decaprio",
         "travelerType": "thrill-seeker"
         },
         {
         "id": 3,
-        "name": "Sibby Dawidowitsch",
+        "name": "Miley Cyrus",
         "travelerType": "shopper"
         },
         {
         "id": 4,
-        "name": "Leila Thebeaud",
+        "name": "My Mom",
         "travelerType": "photographer"
         },
         {
         "id": 5,
-        "name": "Tiffy Grout",
+        "name": "Mel Gibson",
         "travelerType": "thrill-seeker"
         },
     ],
+    trips: [
+        {
+        "id": 1,
+        "userID": 1,
+        "destinationID": 3,
+        "travelers": 1,
+        "date": "2022/09/16",
+        "duration": 8,
+        "status": "approved",
+        "suggestedActivities": []
+        },
+        {
+        "id": 2,
+        "userID": 1,
+        "destinationID": 1,
+        "travelers": 5,
+        "date": "2022/10/04",
+        "duration": 18,
+        "status": "approved",
+        "suggestedActivities": []
+        },
+        {
+        "id": 3,
+        "userID": 3,
+        "destinationID": 2,
+        "travelers": 4,
+        "date": "2022/05/22",
+        "duration": 17,
+        "status": "pending",
+        "suggestedActivities": []
+        }
+    ],
+    destinations: [
+        {
+        "id": 1,
+        "destination": "Chandler, TX",
+        "estimatedLodgingCostPerDay": 70,
+        "estimatedFlightCostPerPerson": 400,
+        "image": "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
+        "alt": "overview of city buildings with a clear sky"
+        },
+        {
+        "id": 2,
+        "destination": "Whitehouse, TX",
+        "estimatedLodgingCostPerDay": 100,
+        "estimatedFlightCostPerPerson": 780,
+        "image": "https://images.unsplash.com/photo-1560089168-6516081f5bf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+        "alt": "city with boats on the water during the day time"
+        },
+        {
+        "id": 3,
+        "destination": "Gainesville, FL",
+        "estimatedLodgingCostPerDay": 130,
+        "estimatedFlightCostPerPerson": 950,
+        "image": "https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+        "alt": "opera house and city buildings on the water with boats"
+        }
+    ]
 }
-
-// export let traveler = {
-//     info: {},
-//     trips: []
-// }
